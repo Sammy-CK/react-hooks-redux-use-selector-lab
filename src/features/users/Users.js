2216d40e0acef66f7d4bf47d1eee039d8b2e695c
Sammy-CK <sammycherono2696@gmail.com>
2023-05-24 16:33:05 +0300

    Integrated redux

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+  const userz = useSelector(state => state.users);
+  const userCount = useSelector(state => state.users).length;
+  
+  let showUsers = userz.map(user => {
+    return(
+    <li key={user.id}>
+      {user.username}
+    </li>
+    )
+  })
+
   return (
     <div>
+      <h3>Users!</h3>
+      <p>Count: {userCount}</p>
       <ul>
-        Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
+        {showUsers}
+        
         {/* In addition, display the total number of users curently in the store */}
+        Total Users: {userCount}
       </ul>
     </div>
   );
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -6,6 +6,7 @@ function usersReducer(state = initialState, action) {
   switch (action.type) {
     case "users/add":
       console.log("adding ", action.payload);
+      console.log(state)
       return {
         ...state,
         users: [...state.users, action.payload],
